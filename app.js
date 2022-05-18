c2aa7e5dc2f843bbf505c45e9993890ce1a35268
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-18 17:27:27 +0300

    adding_style_in_node

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const server = http.createServer((req,res)=>{
     console.log('Server request')
     console.log(req.url,req.method)
 
-    res.setHeader('Content-Type','text/plain')
+    res.setHeader('Content-Type','text/html')//метод может менять тип данных
+        //text/plain; text/html
 
-    res.write('Hello world') //метод для записи
+    res.write('<head><link rel="stylesheet" href="#"/></head>')//добавили стили
+
+    res.write("<h1>Hello world</h1>") //метод для записи
+    res.write("<p>My name is Dima</p>") //метод для записи
     res.end() //метод сообщает что все данные добавлены
 
 });
