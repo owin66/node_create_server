251b48ed1d9a8211dd2e72c1b018f0d00b676187
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-20 14:46:58 +0300

    adding_JSON_file

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const http = require('http')
 
 const PORT = 3000
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     console.log('Server request')
-    console.log(req.url,req.method)
+    console.log(req.url, req.method)
 
     /*res.setHeader('Content-Type','text/html')//метод может менять тип данных
     //text/plain; text/html
@@ -13,11 +13,16 @@ const server = http.createServer((req,res)=>{
 
     res.write("<h1>Hello world</h1>") //метод для записи
     res.write("<p>My name is Dima</p>") //метод для записи*/
+    res.setHeader('Content-Type','application/json')
+    const data = JSON.stringify([
+        {name: 'Tommy', age: 35},
+        {name: 'Arthur', age: 45}
+    ])
 
-    res.end() //метод сообщает что все данные добавлены
+    res.end(data) //метод сообщает что все данные добавлены
 
 });
 
-server.listen(PORT,'localhost',(error)=>{
+server.listen(PORT, 'localhost', (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`)
 });
